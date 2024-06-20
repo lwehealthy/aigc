a85ac6924b2dd684a7682527d50c3bf32b6fe509
liuwei <lyzdliuw@lyzdfintech.com>
2024-06-20 18:59:51 +0800

    init aigc project

---
+++ aigc/study-chatgpt/src/main/java/com/study/studychatgpt/StudyChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.study.studychatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyChatgptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyChatgptApplication.class, args);
+    }
+
+}
---
+++ aigc/study-chatgpt/src/test/java/com/study/studychatgpt/StudyChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studychatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyChatgptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
